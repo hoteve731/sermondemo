743073623ad7a34be71a117e1013e6bc0425d3db
hosan <fbghtks1000@gmail.com>
2023-05-02 20:01:36 -0700

    loginreal

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "edfceec6006f656c"; }
+/******/ 		__webpack_require__.h = function() { return "16446a50b48dbb54"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,14 @@
 // pages/login.js
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../lib/firebase";
-import styles from "../styles/Login.module.css"; 
+import styles from "../styles/Login.module.css";
 
 const Login = () => {
   const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
 
   useEffect(() => {
@@ -32,20 +34,54 @@ const Login = () => {
     }
   };
 
+  const signInWithEmailPassword = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      router.push("/");
+    } catch (error) {
+      console.error("이메일/비밀번호 로그인 실패:", error);
+    }
+  };
+
+  const signUpWithEmailPassword = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      router.push("/");
+    } catch (error) {
+      console.error("이메일/비밀번호 회원가입 실패:", error);
+    }
+  };
+
   return (
     <body>
-        <h1 className="mainlogo">ChatGBD</h1>
-        <h3>돌아오신 걸 환영해요!</h3>
+      <h1 className="mainlogo">ChatGBD</h1>
+      <h3>돌아오신 걸 환영해요!</h3>
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <button onClick={signInWithGoogle}>Google 로그인</button>
+        <div>
+          <button onClick={signInWithGoogle}>Google 로그인</button>
+          <h3>구글 계정이 없다면</h3>
+          <div>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="이메일"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="비밀번호"
+            />
+            <button onClick={signInWithEmailPassword}>로그인</button>
+            <button onClick={signUpWithEmailPassword}>새로 회원가입&로그인</button>
+          </div>
+        </div>
       )}
-        
-      <h6>이 서비스를 사용하기 위해서는 로그인을 해야 합니다!</h6>
 
     </body>
-       
   );
 };
 
